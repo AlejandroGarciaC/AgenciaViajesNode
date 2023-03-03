e744cdecb5bac314b7aea75a1ab497053d57899a
Alejandro Garcia <alejandro.garciaco@outlook.com>
2023-03-02 18:23:10 -0600

    first commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { 
+    paginaInicio, 
+    paginaViajes, 
+    paginaNosotros, 
+    paginaTestimoniales ,
+    paginaDetalleViaje
+} from '../controllers/paginasControler.js';
+
+import { 
+    guardarTestimonial 
+} from '../controllers/testimonialController.js';
+
+const router = express.Router();
+
+router.get('/inicio',paginaInicio);
+
+
+router.get('/nosotros',paginaNosotros);
+
+router.get('/viajes',paginaViajes);
+
+router.get('/viajes/:slug', paginaDetalleViaje);
+
+router.get('/testimoniales',paginaTestimoniales);
+
+router.post('/testimoniales', guardarTestimonial);
+
+
+export default router;
